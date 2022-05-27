8a7c9836e1f5814c3eba4de96186607ee2e63230
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2022-05-27 15:00:25 +0300

    add eks cluster

--- compute/eks.tf
+++ compute/eks.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "eks_cluster" {
-  name = "eks-cluster"
+  name = "eks-cluster-custom"
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -14,4 +14,31 @@ resource "aws_iam_role" "eks_cluster" {
   ]
 }
 POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role = aws_iam_role.eks_cluster.name
+}
+
+resource "aws_eks_cluster" "eks" {
+  name = "eks"
+  role_arn = aws_iam_role.eks_cluster.arn
+  version = "1.21"
+
+  vpc_config {
+    endpoint_private_access = false
+    endpoint_public_access = true
+
+    subnet_ids = [
+        var.public_subnet_a,
+        var.public_subnet_b,
+        var.private_subnet_a,
+        var.private_subnet_b
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
 }
