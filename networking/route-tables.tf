5885a463fd4a4c9e1ce91c45d6098e86a645b9d2
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2022-05-27 13:41:07 +0300

    add routes

--- networking/route-tables.tf
+++ networking/route-tables.tf
@@ -38,3 +38,22 @@ resource "aws_route_table" "eks_private_rtb_02" {
     Name = "eks_private_rtb_02"
   }
 }
+
+
+
+#route table associations
+
+resource "aws_route_table_association" "eks_public_association_a" {
+  subnet_id      = aws_subnet.eks_public_subnet_a.id
+  route_table_id = aws_route_table.eks_public_rtb_01.id
+}
+
+resource "aws_route_table_association" "eks_private_association_a" {
+  subnet_id      = aws_subnet.eks_private_subnet_a.id
+  route_table_id = aws_route_table.eks_private_rtb_01.id
+}
+
+resource "aws_route_table_association" "eks_private_association_b" {
+  subnet_id      = aws_subnet.eks_private_subnet_b.id
+  route_table_id = aws_route_table.eks_private_rtb_02.id
+}
