2daa193127b4854b6fb1bffcdfd6ae3fb4bdd9b9
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2022-05-27 13:00:55 +0300

    add outputs

--- networking/outputs.tf
+++ networking/outputs.tf
@@ -1,3 +1,19 @@
 output "vpcid" {
   value = aws_vpc.eks_vpc.id
 }
+
+output "public_subnet_a" {
+  value = aws_subnet.eks_public_subnet_a.id
+}
+
+output "public_subnet_b" {
+  value = aws_subnet.eks_public_subnet_b.id
+}
+
+output "private_subnet_a" {
+  value = aws_subnet.eks_private_subnet_a.id
+}
+
+output "private_subnet_b" {
+  value = aws_subnet.eks_private_subnet_b.id
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "vpcid" {
   value = module.networking.vpcid
 }
+
+output "public_subnet_a" {
+  value = module.networking.public_subnet_a
+}
+
+output "public_subnet_b" {
+  value = module.networking.public_subnet_b
+}
+
+output "private_subnet_a" {
+  value = module.networking.private_subnet_a
+}
+
+output "private_subnet_b" {
+  value = module.networking.private_subnet_b
+}
