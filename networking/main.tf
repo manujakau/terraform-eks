f1161623cffdaaa00f386e5836a2d9ed21d47e60
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2022-05-27 12:46:21 +0300

    add public subnets

--- networking/main.tf
+++ networking/main.tf
@@ -24,3 +24,28 @@ resource "aws_internet_gateway" "eks_internet_gateway" {
     Name = "eks_igw"
   }
 }
+
+# subnets
+resource "aws_subnet" "eks_public_subnet01" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.subnet_cidrs["public1"]
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "eks_public_a"
+    Tier = "Public"
+  }
+}
+
+resource "aws_subnet" "eks_public_subnet02" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.subnet_cidrs["public2"]
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "eks_public_b"
+    Tier = "Public"
+  }
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -6,7 +6,7 @@ vpc_cidr = "10.0.0.0/16"
 
 subnet_cidrs = {
   public1  = "10.0.1.0/24"
-  public2 = "10.0.2.0/24"
+  public2  = "10.0.2.0/24"
   private1 = "10.0.3.0/24"
   private2 = "10.0.4.0/24"
 }
