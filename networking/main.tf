1fce610f68bab76d46d464e36be3ce2313e4a55e
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2022-05-27 13:13:05 +0300

    add nat gateways

--- networking/main.tf
+++ networking/main.tf
@@ -80,3 +80,47 @@ resource "aws_subnet" "eks_private_subnet_b" {
     "kubernetes.io/role/internal-elb" = 1
   }
 }
+
+
+#Elastic Ip
+resource "aws_eip" "eks_eip_01" {
+  vpc = true
+  depends_on = [
+    aws_internet_gateway.eks_internet_gateway
+  ]
+
+  tags = {
+    Name = "eks_eip_01"
+  }
+}
+
+resource "aws_eip" "eks_eip_02" {
+  vpc = true
+  depends_on = [
+    aws_internet_gateway.eks_internet_gateway
+  ]
+
+  tags = {
+    Name = "eks_eip_02"
+  }
+}
+
+
+#Nat Gateway
+resource "aws_nat_gateway" "eks_nat_gw_01" {
+  allocation_id = aws_eip.eks_eip_01.id
+  subnet_id     = aws_subnet.eks_public_subnet_a.id
+
+  tags = {
+    Name = "eks_nat_gw_01"
+  }
+}
+
+resource "aws_nat_gateway" "eks_nat_gw_02" {
+  allocation_id = aws_eip.eks_eip_02.id
+  subnet_id     = aws_subnet.eks_public_subnet_b.id
+
+  tags = {
+    Name = "eks_nat_gw_02"
+  }
+}
